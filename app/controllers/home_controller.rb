5b475f8a086730654bc64542b8cde79cdb8626fd
shane <shaneob121@hotmail.com>
2013-12-02 09:31:25 +0000

    added subs to pitch view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   require 'ReadLogs'
   def index
-    readlogs = ::ReadLogs.new
-    @readlogs_div_tag = readlogs.read('development.log')
+    # uncomment to show logs
+    #readlogs = ::ReadLogs.new
+    #@readlogs_div_tag = readlogs.read('development.log')
   end
 end
